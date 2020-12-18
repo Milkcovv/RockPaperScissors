307ca19bd10ca52f8aeb68d6293df94f51e5f1e5
Hui <hui357786325@gmail.com>
2020-12-18 10:06:40 +0800

    done

--- index.js
+++ index.js
@@ -1,49 +1,86 @@
-let choice = ['rock','paper', 'scissors']
+const choice = ['rock','paper', 'scissors']
 let computerScore = 0;
 let humanScore = 0;
+let playerChoice = ''
+let computerchoice = ''
+
+
+
+const choiceBtn = document.querySelectorAll('button');
+const resultTag = document.querySelector('h1');
+const humanScoreTag = document.querySelector('#humanScore');
+const computerScoreTAg = document.querySelector('#computerScore');
+
+
 function computerPlay(){
   let randomNumber = Math.floor(Math.random()*3)
   return choice[randomNumber]
 }
 
 
-function playRound(playerSelection, computerSelection) {
-  let a = playerSelection.toLowerCase()
-  let b = computerSelection.toLowerCase()
-  if ((a == 'rock' && b == 'scissors') ||
+
+function playRound(playerChoice) {
+  let a = playerChoice.target.id;
+  let b = computerPlay();
+
+  if(humanScore < 5 && computerScore <5)
+  {
+      if ((a == 'rock' && b == 'scissors') ||
         (a == 'scissors' && b == 'paper') ||
         (a == 'paper' && b == 'rock')) 
       {
       humanScore++;
-      return(`You won! ${a} beats ${b}`)
+      humanScoreTag.textContent = `Your Score: ${humanScore}`;
+      resultTag.textContent = `You won! ${a} beats ${b}`
         }
     else if(a == b){
-      return("It is a tie")
+      resultTag.textContent ="It is a tie";
     } else{
        computerScore++;
-            return(`Computer won! ${b} beats ${a}`)
-           
-
+       computerScoreTAg.textContent = `Computer Score: ${computerScore}`;
+       resultTag.textContent =`Computer won! ${b} beats ${a}`;
     }
-
+  }else{
+    alert(`${Math.max(computerScore>humanScore)?"Computer Won!":"Human Won!"}`)
+    computerScore = 0;
+    humanScore = 0;
+    computerScoreTAg.textContent = `Computer Score: ${computerScore}`;
+    humanScoreTag.textContent = `Your Score: ${humanScore}`;
   }
 
-function gameRound(){
-  let count = 0;
-  while (count < 5) {
-    const playerSelection = prompt('make a choice')
-    const computerSelection = computerPlay();
-    console.log(`computer chose ${computerSelection}`)
-    console.log(playRound(playerSelection, computerSelection));
-    count++;
+    
   }
-  console.log(`computer won ${computerScore} games`)
-  console.log(`human won ${humanScore} games`)
-  console.log(` ${Math.abs(humanScore + computerScore -5)} tie games`)
 
-  humanScore > computerScore ? console.log(`You won ${humanScore} games`): console.log(`Computer won ${computerScore} games`)
+
+choiceBtn.forEach(btn=>{
+  btn.addEventListener('click', playRound)
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function gameRound(){
+//   let count = 0;
+//     const computerSelection = computerPlay();
+//     console.log(`computer chose ${computerSelection}`)
+//     console.log(playRound(playerSelection, computerSelection));
+//     count++;
+//   }
+//   console.log(`computer won ${computerScore} games`)
+//   console.log(`human won ${humanScore} games`)
+//   console.log(` ${Math.abs(humanScore + computerScore -5)} tie games`)
+
+//   humanScore > computerScore ? console.log(`You won ${humanScore} games`): console.log(`Computer won ${computerScore} games`)
 
   
-}
+// }
 
-gameRound()
